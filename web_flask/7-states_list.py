12a392ec0946887cea46edd2b72af440cc8ddf14
Nne85 <nneukamaka@yahoo.com>
2023-07-22 06:23:37 -0700

    Task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application.
+Starts a Flask web application that displays a list of States.
 """
 
-from models import storage
-from models.state import State
 from flask import Flask, render_template
+from models import storage, State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(error):
-    """Closes the database again at the end of the request."""
+def teardown_session(exception):
+    """Closes the current SQLAlchemy Session."""
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display HTML page with the list of all State objects sorted by name."""
-    states = storage.all(State).values()
+    """Displays a HTML page with the list of all State objects."""
+    states = storage.all("State")
     return render_template('7-states_list.html', states=states)
 
 
